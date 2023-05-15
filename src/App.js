d7774a2031d6e6436e210c9e20c530d677b6025e
youngyun213 <k12377733@gmail.com>
2023-05-15 16:37:11 +0900

    헤더 생성

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Main from "./pages/Main";
+import ProductList from "./pages/ProductList";
+import Bookmark from "./pages/Bookmark";
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
       <Footer />
-    </>
+
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/productlist" element={<ProductList />} />
+        <Route path="/bookmark" element={<Bookmark />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,51 @@
+import styled from "styled-components";
+
+const Header_body = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  position: sticky;
+  top: 0;
+
+  height: 80px;
+
+  padding: 25px 75px;
+  background: #ffffff;
+  box-shadow: 0px 8px 8px rgba(0, 0, 0, 0.1);
+
+  > #title_container {
+    display: flex;
+    align-items: center;
+
+    > h1 {
+      padding-left: 12px;
+    }
+
+    > span > #logo {
+      width: 55px;
+    }
+  }
+
+  > span > #ham_icon {
+    width: 30px;
+  }
+`;
+
 function Header() {
-  return <header>header</header>;
+  return (
+    <Header_body>
+      <div id="title_container">
+        <span>
+          <img id="logo" src="../logo.png" alt="logo" />
+        </span>
+        <h1>COZ Shopping</h1>
+      </div>
+      <span>
+        <img id="ham_icon" src="../icon.png" alt="햄버거 버튼" />
+      </span>
+    </Header_body>
+  );
 }
 
 export default Header;

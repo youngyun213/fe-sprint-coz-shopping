313b08fb51b292e1dc0158f99cf243166dfcf0eb
youngyun213 <k12377733@gmail.com>
2023-05-15 17:29:01 +0900

    footer 구현

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,26 @@
+import styled from "styled-components";
+
+const Footer_body = styled.footer`
+  text-align: center;
+  color: #888888;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  height: 58px;
+  background: #ffffff;
+  border-top: 1px solid rgba(0, 0, 0, 0.1);
+`;
+
 function Footer() {
-  return <footer>footer</footer>;
+  return (
+    <Footer_body>
+      개인정보 처리방침 | 이용 약관
+      <br />
+      All rights reserved @ Codestates
+    </Footer_body>
+  );
 }
 
 export default Footer;
